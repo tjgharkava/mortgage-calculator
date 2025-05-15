86e946af6218ecb3d4cfd78e81b8de3964c7afb2
Temo <tdzhgarkava@yahoo.com>
2025-05-15 18:00:03 +0400

    last commit

--- src/main/java/ge/temo/Main.java
+++ src/main/java/ge/temo/Main.java
@@ -20,17 +20,21 @@ public class Main {
         while (true) {
             System.out.print("Annual Interest Rate: ");
             float annualInterest = scanner.nextFloat();
-            if (annualInterest > 0 && annualInterest <= 30) {
+            if (annualInterest > 0 && annualInterest <= 30)
                 break;
-            }
             System.out.println("Enter a value between 1 and 30");
             monthlyInterest = annualInterest / 100 / 12;
         }
 
-
-        System.out.print("Period: ");
-        byte years = scanner.nextByte();
-        int numOfPayments = years * 12;
+        int numOfPayments = 0;
+        while (true) {
+            System.out.print("Period: ");
+            byte years = scanner.nextByte();
+            if (years > 0 && years <= 12)
+                break;
+            System.out.println("Enter a value between 1 and 12");
+            numOfPayments = years * 12;
+        }
 
         double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numOfPayments)) / (Math.pow(1 + monthlyInterest, numOfPayments) - 1);
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
